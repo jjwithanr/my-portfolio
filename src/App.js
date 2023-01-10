02f2abeab9032019b4bc7c7718bf6a16ba05bcca
jjwithanr <jeremychang623@gmail.com>
2023-01-09 23:41:04 -0800

    Display Taskbar and StartMenu component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { styleReset } from 'react95';
+import Taskbar from './components/Taskbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* Pick a theme of your choice */
+import original from 'react95/dist/themes/original';
+
+/* Original Windows95 font (optional) */
+import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
+import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+import { RecoilRoot } from 'recoil';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal
+  }
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal
+  }
+  body, input, select, textarea {
+    font-family: 'ms_sans_serif';
+  }
+  ${styleReset}
+`;
+
+const App = () => (
+  <RecoilRoot>
+    <GlobalStyles />
+    <ThemeProvider theme={original}>
+      <Taskbar />
+    </ThemeProvider>
+  </RecoilRoot>
+);
 
 export default App;
