2ceda64300dd0fb48588bdd509a5e6d5fa470312
jjwithanr <jeremychang623@gmail.com>
2023-01-11 22:27:00 -0800

    Implement taskbar clock

---
+++ src/components/Taskbar/clock.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function TaskbarClock() {
+    const refClock = React.useRef(undefined);
+    const refTimer = React.useRef(0);
+
+    const clock = () => {
+        const addZero = (t) => {
+            if (t < 10) return `0${t}`;
+            return t;
+        };
+        const date = new Date();
+        let hour = date.getHours();
+        const min = addZero(date.getMinutes());
+        const interval = (60 - date.getSeconds()) * 1000 + 5;
+        const formatHour = (h) => {
+            if ( h > 12 ) h = addZero(h - 12);
+            return h;
+        }
+
+        refTimer.current = window.setTimeout(() => {
+            clock();
+        }, interval);
+
+        if (hour > 12)
+            refClock.current.innerHTML = `${formatHour(hour)}:${min} PM`;
+        else
+            refClock.current.innerHTML = `${formatHour(hour)}:${min} AM`;
+    };
+
+    React.useEffect(() => {
+    refClock && refClock.current && clock();
+    return () => {
+        window.clearTimeout(refTimer.current);
+        refClock.current = null;
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return <p ref={refClock} className="taskbarClock"></p>;
+}
--- src/components/Taskbar/index.js
+++ src/components/Taskbar/index.js
@@ -1,13 +1,17 @@
 import { AppBar, Toolbar } from 'react95';
 import "./styles.scss";
 import StartMenu from '../StartMenu';
+import TaskbarClock from './clock';
 
 export default function Taskbar() {
     return (
         <AppBar className="taskbar">
-        <Toolbar style={{ justifyContent: 'space-between' }}>
+        <Toolbar>
             <StartMenu />
         </Toolbar>
+
+        <TaskbarClock />
+
         </AppBar>
     );
 }
