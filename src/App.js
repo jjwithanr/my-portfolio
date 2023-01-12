35fa344658f2239425f6a439640e8b659eb4f167
jjwithanr <jeremychang623@gmail.com>
2023-01-11 22:26:44 -0800

    Import pixelated Windows 95 font

--- src/App.js
+++ src/App.js
@@ -2,30 +2,16 @@ import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { styleReset } from 'react95';
 import Taskbar from './components/Taskbar';
+import './App.css';
 
-/* Pick a theme of your choice */
 import original from 'react95/dist/themes/original';
 
-/* Original Windows95 font (optional) */
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+import "./fonts/w95fa.woff";
 import { RecoilRoot } from 'recoil';
 
 const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif}') format('woff2');
-    font-weight: 400;
-    font-style: normal
-  }
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif_bold}') format('woff2');
-    font-weight: bold;
-    font-style: normal
-  }
-  body, input, select, textarea {
-    font-family: 'ms_sans_serif';
+  * {
+    font-family: "Pixel" !important;
   }
   ${styleReset}
 `;
