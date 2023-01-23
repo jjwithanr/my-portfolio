38a5c16eed8f968891fa2053cfdaab7ad35cbfad
jjwithanr <jeremychang623@gmail.com>
2023-01-22 22:52:44 -0800

    Add windows 95 scrollbar to About window

--- src/windows/About/index.js
+++ src/windows/About/index.js
@@ -7,9 +7,20 @@ export default function About() {
 
     return (
         <section className="aboutWindow">
-            <div className="scrollable -yOnly aboutWindow__body">
+            <div className="scrollable aboutWindow__body">
                 <div className="aboutWindow__bodyInner">
-                    <h1 className="mb2">ABOUT</h1>
+                    <h1 className="mb2">Welcome to my portfolio website!</h1>
+                    <p className="mb2">
+                        I'm Jeremy Chang and I am a UCI graduate with a B.S. in Computer Engineering
+                        with a minor in Information & Computer Science.
+                    </p>
+
+                    <img src={require("../../assets/profile-picture.jpg")} />
+
+                    <p>
+                        Based in L.A., I'm an apiring full-stack developer and am passionate about 
+                        software developement and am open to entry-level roles in the tech industry.
+                    </p>
                 </div>
             </div>
         </section>
