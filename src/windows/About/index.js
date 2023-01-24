05761d9c86ad5776d507b4512f7bc466f4002125
jjwithanr <jeremychang623@gmail.com>
2023-01-23 16:25:18 -0800

    Finish simple about window

--- src/windows/About/index.js
+++ src/windows/About/index.js
@@ -8,19 +8,22 @@ export default function About() {
     return (
         <section className="aboutWindow">
             <div className="scrollable aboutWindow__body">
+                    <h1>Welcome to my portfolio website!</h1>
                 <div className="aboutWindow__bodyInner">
-                    <h1 className="mb2">Welcome to my portfolio website!</h1>
-                    <p className="mb2">
-                        I'm Jeremy Chang and I am a UCI graduate with a B.S. in Computer Engineering
-                        with a minor in Information & Computer Science.
-                    </p>
-
-                    <img src={require("../../assets/profile-picture.jpg")} />
-
-                    <p>
-                        Based in L.A., I'm an apiring full-stack developer and am passionate about 
-                        software developement and am open to entry-level roles in the tech industry.
-                    </p>
+                    <div className="description">
+                        <p>
+                            I'm Jeremy Chang and I graduated from UC Irvine in 2022 with a B.S. in Computer Engineering
+                            with a minor in Information & Computer Science.
+                        </p>
+                        <br />
+                        <p>
+                            Based in L.A., I'm an apiring full-stack developer and am passionate about 
+                            software developement. I am currently open to entry-level software roles!
+                        </p>
+                    </div>
+                    <div className="picture"> 
+                        <img src={require("../../assets/profile-picture.jpg")} />
+                    </div>
                 </div>
             </div>
         </section>
--- src/windows/Resume/index.js
+++ src/windows/Resume/index.js
@@ -1,17 +1,32 @@
 import React from "react";
-import { Button, Anchor } from "react95";
-
 import "./styles.scss";
+import myResume from "./Chang_Jeremy_Resume.pdf"
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function Resume() {
 
+    const [numPages, setNumPages] = React.useState(null);
+    const [pageNumber, setPageNumber] = React.useState(1); //setting 1 to show fisrt page
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
     return (
-        <section className="resumeWindow">
-            <div className="scrollable -yOnly resumeWindow__body">
-                <div className="resumeWindow__bodyInner">
-                    <h1 className="mb2">Resume</h1>
-                </div>
-            </div>
-        </section>
+        // <section className="resumeWindow">
+        //     <div className="scrollable -yOnly resumeWindow__body">
+        //         {/* <div className="resumeWindow__bodyInner">
+        //             <h1 className="mb2">Resume</h1>
+        //         </div> */}
+        //     </div>
+        // </section>
+                <Document
+                    file="/src/windows/Resume/Chang_Jeremy_Resume.pdf"
+                    onLoadSuccess={onDocumentLoadSuccess}
+                >
+                    <Page pageNumber={numPages} />
+                </Document>
     );
 }
