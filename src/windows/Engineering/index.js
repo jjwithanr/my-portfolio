0bb6703b5b20151dac01174268f3269ea215a51a
jjchang623@yahoo.com <jrchang@jrchang180480.local>
2023-06-19 11:13:56 -0700

    Adjust sizing and copy of all windows

--- src/windows/Engineering/index.js
+++ src/windows/Engineering/index.js
@@ -32,10 +32,10 @@ export default function Engineering() {
                     <div className="scrollable engineeringWindow__details">
                     <div className="engineeringWindow__item">
                         <img
-                        src={require("../../assets/media/virtual-quadcopter.png")}
-                        alt=""
-                        width="400"
-                        style={{ marginRight: "20px" }}
+                            src={require("../../assets/media/virtual-quadcopter.png")}
+                            alt=""
+                            width="35%"
+                            style={{ marginRight: "20px" }}
                         />
                         <p>
                         The quadcopter engineering project provided valuable hands-on experience applying fundamental design processes from concept creation to prototype testing and refinement. I gained skills in teamwork, problem-solving, and project management that will translate well to an engineering role.
@@ -44,10 +44,10 @@ export default function Engineering() {
                     </div>
                     <div className="engineeringWindow__item">
                         <img
-                        src={require("../../assets/media/irl-quadcopter.JPG")}
-                        alt=""
-                        width="400"
-                        style={{ marginRight: "20px" }}
+                            src={require("../../assets/media/irl-quadcopter.JPG")}
+                            alt=""
+                            width="35%"
+                            style={{ marginRight: "20px" }}
                         />
                         <p>
                         Working through each stage of the project helped me strengthen valuable technical and soft skills through practical application. I learned to break down complex design challenges, collaborate well in an interdisciplinary team, and organize tasks to deliver results on time and on budget. The hands-on experience building the quadcopter increased my confidence in my ability to tackle ambitious engineering projects, solve problems creatively, and produce high-quality work that meets key requirements.   
@@ -66,33 +66,30 @@ export default function Engineering() {
                     <div className="scrollable engineeringWindow__details">
                     <div className="engineeringWindow__item">
                         <img
-                        src={require("../../assets/media/irl-fitness-watch.jpg")}
-                        alt=""
-                        width="400"
-                        style={{ marginRight: "20px" }}
+                            src={require("../../assets/media/irl-fitness-watch.jpg")}
+                            alt=""
+                            width="25%"
+                            style={{ marginRight: "20px" }}
                         />
                         <p>
-                        The objective of the project is to create an affordable and practical fitness tracker. To do so, we followed
-                        a specific process of planning, designing, coding, and fabricating. Programs such as TinkerCAD, SolidWorks,
-                        MIT App Inventor were necessary for the creation of the device. Each member of the group was required to
-                        collaborate and participate in the engineering process of the project.
+                        The goal of this project was to develop an affordable, practical fitness tracker. We followed an iterative engineering design process of planning, prototyping, testing and refinement. Software like TinkerCAD, SolidWorks and MIT App Inventor were essential tools for our product development. Collaboration between members was critical, with each member contributing their unique skills and perspectives throughout the process.
                         </p>
                     </div>
                     <div className="engineeringWindow__item">
-                        <img src={require("../../assets/media/circuit-boards.png")} alt="" width="400" style={{ marginRight: "20px" }} />
+                        <img 
+                            src={require("../../assets/media/circuit-boards.png")} 
+                            alt="" 
+                            width="25%" 
+                            style={{ marginRight: "20px" }} 
+                        />
                         <p>
-                        From the beginning, we aimed to reduce the thickness of our fitness watch as much as possible. We decided on
-                        a modular design as a solution to avoiding a chunky all-in-one design. In Solidworks, we created modules with
-                        different specifications to contain different components of the fitness watch. As a team, we agreed that 3D
-                        printing these parts was the most effective method in ensuring our fitness watch matched our Solidworks design
-                        measurements. After printing several iterations, we saw an opportunity to minimize even more thickness and
-                        space in our fitness watch. We shortened the lengths and widths of one of our modulesto be just big enough to hold the electrical components leaving no excess space. The final print and assembly came out beautifully with the teamwork of our amazing diverse team.
+                        To reduce thickness, we adopted a modular design instead of an all-in-one approach. In Solidworks, we designed custom modules to house different electrical components. 3D printing provided the precision needed to match our module designs. Through multiple iterations, we identified opportunities to further minimize dimensions while still fitting components. Shortening module lengths and widths resulted in a more compact design while maintaining functionality.
                         </p>
                     </div>
                     <div className="engineeringWindow__item">
                         <img src={require("../../assets/media/app-fitness-watch.png")} alt="" width="400" style={{ marginRight: "20px" }} />
                         <p>
-                        In most areas our prototype performed up to or exceeded our predictions. In terms of weight and comfort our prototype performed better than predicted. Our design came out to be quite light, only slightly heavier than a standard fitness band. Our prototype was also comfortable on the wrist due to its adjustable nylon band, lightweight components, and distributed weight. However, due to its width across the band, it did not sit very securely on slim wrists. Perhaps if we reduced the space between modules a bit more we could have accounted for this issue. Aesthetically, due to our polycarbonate lids, clean prints and clean soldering, our model was pleasing to the eye, exceeding our expectations in that regard. In terms of durability of the device itself we did not run into any issues. Our supports from the 3D print were solid and secure and were strong enough to withstand wear and tear from standard daily functions.
+                        Our prototype met or exceeded expectations in several areas. It was lighter weight and more comfortable than expected thanks to an adjustable band, lightweight materials and distributed weight. However, its width caused issues for slimmer wrists. Future iterations could reduce module spacing to address this. In summary, the project provided invaluable experience developing an engineering solution through team-based planning, prototyping and testing. The iterative process helped refine our design to meet critical size, weight and functionality targets while identifying areas for future improvement.
                         </p>
                     </div>
                     <div className="engineeringWindow__item">
